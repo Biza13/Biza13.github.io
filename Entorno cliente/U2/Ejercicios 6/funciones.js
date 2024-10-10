091cdce93272fd6a648a8b78af8458d4b537ec90
Biza_13 <serrano_0_1@hotmail.com>
2024-10-10 18:29:51 +0200

    prueba de jsdoc

---
+++ Entorno cliente/U2/Ejercicios 6/funciones.js
@@ -0,0 +1,161 @@
+/**
+ * Funcion que muestra una ventana emergente con dos opciones y segun la que pulses te da un mensaje u otro
+ */
+function confirmacion(){
+    let confirmar = confirm("aceptar o rechazar");
+    if (confirmar == true){
+        alert("Has aceptado")
+    }else{
+        alert("Has cancelado")
+    }
+}
+
+/**
+ * funcion que abre una ventana emergente
+ */
+function abrirVentana() {
+    ventana = window.open("", "miVentana", `width=350,height=350, top=0`);
+}
+
+/**
+ * Funcion que cierra una ventana emergente
+ */
+function cerrarVentana(){
+    ventana.close();
+}
+
+/**
+ * Funcion que responde a si una ventana emergente esta abierta o no
+ */
+function estaLaVentanaAbierta(){
+    //si ventana esta abierta(ventana) y ventana NO se ha cerrado
+    if (ventana && !ventana.closed){
+        alert("La ventana esta abierta");
+    }else{
+        alert("La ventana esta cerrada");
+    }
+}
+
+/**
+ * Funcion para abrir una ventana emergente y ponerle un titulo
+ * @param {String} titulo 
+ */
+function abrirVentanaTitulo(titulo) {
+    let tit = titulo;
+    ventana = window.open("", "miVentana", `width=350,height=350, top=0`);
+    
+    ventana.document.write(
+      `<!DOCTYPE html>
+      <html lang="es">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Ejercicio 2</title>
+      </head>
+      <body>
+          <h1>${tit}</h1>
+      </body>
+      </html>`
+    );
+}
+
+/**
+ * Funcion para cerrar la ventana actual
+ */
+function cerrarVentanaActual(){
+    window.close();
+}
+
+/**
+ * Funcion para abrir una ventana emergente con un tamaño especifico
+ * @param {Number} alto 
+ * @param {number} ancho 
+ */
+function abrirVentanaConTamaño (alto=300, ancho=100){
+    ventana = window.open("", "miVentana", `width=${ancho},height=${alto}, top=0`);
+    ventana.write(
+        `<!DOCTYPE html>
+      <html lang="es">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Ejercicio 2</title>
+      </head>
+      <body>
+        
+      </body>
+      </html>`
+    );
+}
+/**
+ * Funcion que escribe un texto en la clase definida
+ * @param {String} texto 
+ */
+function escribe(texto){
+    document.getElementById("parrafo").innerHTML = texto;
+}
+
+/**
+ * Funcion que crea una ventana emergente en la posicion que le digas, si no le dices nada por defecto saldra a 50 pixeles en los dos ejes
+ * @param {number} x 
+ * @param {number} y 
+ */
+function moverVentana(x=50,y=50){
+    ventana.moveTo(x,y);
+}
+
+/**
+ * Función para ir a un marcador puesto en elgun punto de la pagina
+ */
+function irAlMarcador(){
+    location.hash = "marca_dor";
+}
+
+/**
+ * Función para saber el nombre del marcador
+ */
+function nombreMarcador(){
+    alert("El marcador se llama: marca_dor");
+}
+
+/**
+ * función para ir a una url dada por el susario
+ * @param {string} url 
+ * @returns 
+ */
+function irA(url){
+    if (!url.startsWith("https://") && !url.startsWith("http://")){
+        alert("Ingrese tambien el https://www... o http://www...");
+        return; //sale de la funcion si la url no es valida
+    }
+    location.href = url;
+}
+
+/**
+ * Funcion que actualiza la fecha de la ultima vez que se refresco la pagina
+ */
+function actualizarFecha(){
+    let dat = new Date();
+    let hor = dat.getHours();
+    let min = dat.getMinutes();
+    let seg = dat.getSeconds();
+    let fech = dat.toDateString();
+    document.getElementById("parrafo").innerHTML = fech + "<br>" + hor + ":" + min + ":" + seg;
+}
+
+/**
+ * funcion para ver de que paginas has accedido a esta
+ */
+function deDondeViene(){
+    let pag = document.referrer;
+    if (pag == ""){
+        document.getElementById("referencia").innerHTML = "Has entrado a la página directamente";
+    }else{
+        document.getElementById("referencia").innerHTML = pag;
+    }
+}
+
+function enseñarTitulo(){
+    let titulo = document.title;
+    document.getElementById("titulo").innerHTML = titulo;
+}
